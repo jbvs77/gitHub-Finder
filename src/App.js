1d2b005a4a2bf74dba41c3ffc659aa65fb5a3f6a
Luis Gomez <vaquezgomezluisfernando@gmail.com>
2023-02-20 22:24:03 -0600

    2nd push - data from API fetched

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Layout/Navbar";
 import Footer from "./Components/Layout/Footer";
+import About from "./Pages/About";
+import NotFound from "./Pages/NotFound";
+import Home from "./Pages/Home";
 function App() {
   return (
-    <Router className="App">
+    <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main className="container mx-auto px-3 pb-12">Content</main>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UserResults from "../Components/User/UserResults";
+
+function Home() {
+  return (
+    <div>
+      {/*SEARCH COMPONENT */}
+      <UserResults />
+    </div>
+  );
+}
+
+export default Home;
